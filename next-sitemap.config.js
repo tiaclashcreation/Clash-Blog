369df0b64f5a67909e7be76cffd42e5b4eb4870e
Tia Clash Creation <tia@clashcreation.com>
2025-05-17 15:56:18 +0100

    updated meta data for subdomain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://clashcreation.com/blog',
+  siteUrl: 'https://blog.clashcreation.com',
   generateRobotsTxt: true,
   outDir: './public',
 }; 
